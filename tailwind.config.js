6de3c5c51fb1d0080360dbd11df6eac70711e672
luisfigueras <8luisfigueras@gmail.com>
2022-07-12 13:14:29 +0200

    *

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    "./resources/views/auth/passwords/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('@tailwindcss/forms')
+    ],
+
+}
+
+
